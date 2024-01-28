d38dd4bd1481b8fe5034118556b4f3520a8a9ab9
Julian <thejulianjuko@gmail.com>
2024-01-28 19:23:19 +0100

    base image setup

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,6 +3,18 @@
 
 fn main() {
   tauri::Builder::default()
+  .invoke_handler(tauri::generate_handler![read_image])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
+}
+
+#[tauri::command]
+fn read_image(file_path: String) -> Result<String, String> {
+    match std::fs::read(file_path) {
+        Ok(bytes) => {
+            let base64_encoded = base64::encode(bytes);
+            Ok(format!("data:image/png;base64,{}", base64_encoded))
+        }
+        Err(e) => Err(e.to_string()),
+    }
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,33 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import { invoke } from "@tauri-apps/api/tauri";
 
 function App() {
+	const [imageSrc, setImageSrc] = useState("");
+
+	async function loadImage() {
+		try {
+			const base64Image: React.SetStateAction<string> = await invoke(
+				"read_image",
+				{
+					filePath:
+						"C:\\Users\\theju\\Pictures\\Screenshots\\gumos.png",
+				}
+			);
+			setImageSrc(base64Image);
+		} catch (error) {
+			console.error("Failed to load image", error);
+		}
+	}
+
+	useEffect(() => {
+		loadImage();
+	}, []);
+
 	return (
-		<>
-			<h1>Image Viewer</h1>
-		</>
+		<div>
+			{imageSrc ? <img src={imageSrc} alt="Loaded" /> : <p>Loading...</p>}
+		</div>
 	);
 }
 
