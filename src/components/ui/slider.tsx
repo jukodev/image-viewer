ffc56cbbe097d1a55ff1aaea2863e19ddb1aad31
Julian <thejulianjuko@gmail.com>
2024-02-01 18:50:42 +0100

    added slider text

--- src/components/ui/slider.tsx
+++ src/components/ui/slider.tsx
@@ -9,18 +9,23 @@ const Slider = React.forwardRef<
 	React.ElementRef<typeof SliderPrimitive.Root>,
 	React.ComponentPropsWithoutRef<typeof SliderPrimitive.Root>
 >(({ className, ...props }, ref) => (
-	<SliderPrimitive.Root
-		ref={ref}
-		className={cn(
-			"relative flex w-full touch-none select-none items-center",
-			className
-		)}
-		{...props}>
-		<SliderPrimitive.Track className="relative h-1.5 w-full grow overflow-hidden rounded-full bg-primary/20">
-			<SliderPrimitive.Range className="absolute h-full bg-primary" />
-		</SliderPrimitive.Track>
-		<SliderPrimitive.Thumb className="block h-4 w-4 rounded-full border border-primary/50 bg-background shadow transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50" />
-	</SliderPrimitive.Root>
+	<>
+		<p className="absolute bottom-0 right-1/4 mx-7 mb-2">
+			{Math.round(props.value![0] * 100)}%
+		</p>
+		<SliderPrimitive.Root
+			ref={ref}
+			className={cn(
+				"relative flex w-full touch-none select-none items-center",
+				className
+			)}
+			{...props}>
+			<SliderPrimitive.Track className="relative h-1.5 w-full grow overflow-hidden rounded-full bg-primary/20">
+				<SliderPrimitive.Range className="absolute h-full bg-primary" />
+			</SliderPrimitive.Track>
+			<SliderPrimitive.Thumb className="block h-4 w-4 rounded-full border border-primary/50 bg-background shadow transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50" />
+		</SliderPrimitive.Root>
+	</>
 ));
 Slider.displayName = SliderPrimitive.Root.displayName;
 
